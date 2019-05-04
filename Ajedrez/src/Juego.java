36c4aaf7f022de858bf290e1860bdff9e35e18e1
Valeria Rodriguez <valery-rod@hotmail.com>
2019-05-04 02:52:06 -0500

    finished the chess

--- Ajedrez/src/Juego.java
+++ Ajedrez/src/Juego.java
@@ -9,15 +9,35 @@
  * @author valeria
  */
 public class Juego {
-    
-    //private int turno;
+
+    private int turno;
     private String[][] tablero;
-    
+    private boolean movimiento;
+
     public Juego() {
         tablero = new String[8][8];
+        turno = 0;
+        movimiento = false;
         inicializarTablero();
     }
-    
+
+    //turno
+    public int getTurno() {
+        return turno;
+    }
+
+    public void setTurno(int turno) {
+        this.turno = turno;
+    }
+
+    public boolean isMovimiento() {
+        return movimiento;
+    }
+
+    public void setMovimiento(boolean movimiento) {
+        this.movimiento = movimiento;
+    }
+
     private void inicializarTablero() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
@@ -27,7 +47,7 @@ public class Juego {
         inicializarBlancas();
         inicializarNegras();
     }
-    
+
     private void inicializarBlancas() {
         tablero[7][1] = "CB";
         tablero[7][6] = "CB";
@@ -37,7 +57,7 @@ public class Juego {
             tablero[6][i] = "PB";
         }
     }
-    
+
     private void inicializarNegras() {
         tablero[0][1] = "CN";
         tablero[0][6] = "CN";
@@ -47,55 +67,71 @@ public class Juego {
             tablero[1][i] = "PN";
         }
     }
-    
-    private void moverFicha(int i1, int j1, int i2, int j2) {
-        if (tablero[i1][j1].isEmpty()) {
-            System.out.println("No hay ficha");
+
+    public void moverFicha(int i1, int j1, int i2, int j2) {
+        movimiento = false;
+        if (tablero[i1][j1].contains(" ")) {
+            System.out.println("No hay ficha en la posición actual");
         } else {
-            String caso = mirarFicha(tablero[i1][j1]);
-            if (tablero[i1][j1].contains("N")) {
-                sePuedeMover(11, j1, i2, j2);
+            if (tablero[i1][j1].contains("N") && (turno % 2 != 0)) {
+                sePuedeMover(i1, j1, i2, j2);
+            } else if (tablero[i1][j1].contains("B") && (turno % 2 == 0)) {
+                sePuedeMover(i1, j1, i2, j2);
             } else {
-                
+                System.out.println("La posición actual no es de su color");
             }
         }
+
     }
-    
-    private String mirarFicha(String ficha) {
-        if (ficha.contains("A")) {
-            return "alfil";
-        } else if (ficha.contains("C")) {
-            return "caballo";
-        }
-        return "peon";
-    }
-    
+
     private void sePuedeMover(int i1, int j1, int i2, int j2) {
         String ficha = tablero[i1][j1];
         if (ficha.contains("A")) {
             if (moverAlfil(i1, j1, i2, j2)) {
-                System.out.println("Movimiento realizado");
+                System.out.println("Movimiento del alfil realizado");
+                movimiento = true;
+                turno++;
             } else {
-                System.out.println("El movimiento no es posible"); //aca toca pensar como hacer para mantenerse repitiendo
+                System.out.println("El movimiento del alfil no es posible");
             }
         } else if (ficha.contains("C")) {
             if (ficha.contains("N")) {
                 if (moverCaballo(i1, j1, i2, j2, "N")) {
-                    
+                    System.out.println("Movimiento del caballo realizado");
+                    movimiento = true;
+                    turno++;
+                } else {
+                    System.out.println("El movimiento del caballo no es posible");
                 }
             } else {
                 if (moverCaballo(i1, j1, i2, j2, "B")) {
-                    
+                    System.out.println("Movimiento del caballo realizado");
+                    movimiento = true;
+                    turno++;
+                } else {
+                    System.out.println("El movimiento del caballo no es posible");
                 }
             }
-            
+
         } else if (ficha.equals("PN")) {
-            moverPeonNegro(i1, j1, i2, j2);
+            if (moverPeonNegro(i1, j1, i2, j2)) {
+                System.out.println("Movimiento del peon realizado");
+                movimiento = true;
+                turno++;
+            } else {
+                System.out.println("El movimiento del peon no es posible");
+            }
         } else if (ficha.equals("PB")) {
-            moverPeonBlanco(i1, j1, i2, j2);
+            if (moverPeonBlanco(i1, j1, i2, j2)) {
+                System.out.println("Movimiento del peon realizado");
+                movimiento = true;
+                turno++;
+            } else {
+                System.out.println("El movimiento del peon no es posible");
+            }
         }
     }
-    
+
     private boolean moverAlfil(int i1, int j1, int i2, int j2) {
         int maximoI = Math.max(i1, i2);
         int maximoJ = Math.max(j1, j2);
@@ -103,14 +139,61 @@ public class Juego {
         int minimoJ = Math.min(j1, j2);
         if ((maximoI - minimoI) == (maximoJ - minimoJ)) {
             if (tablero[i1][j1].contains("N")) {
-                return puedeMoverAlfil(i1, j1, i2, j2, "N", "B");
+                if (recorridoLibre(i1, j1, i2, j2, (maximoI - minimoI),"B")) {
+                    return puedeMoverAlfil(i1, j1, i2, j2, "N", "B");
+                }
             } else {
-                return puedeMoverAlfil(i1, j1, i2, j2, "B", "N");
+                if (recorridoLibre(i1, j1, i2, j2, (maximoI - minimoI),"N")) {
+                    return puedeMoverAlfil(i1, j1, i2, j2, "B", "N");
+                }
             }
         }
         return false;
     }
-    
+
+    private boolean recorridoLibre(int i1, int j1, int i2, int j2, int totalRecorrer, String enemigo) {
+        //casos
+        System.out.println("toal: "+(totalRecorrer-1));
+        boolean atravesado = false;
+        if (i1 < i2 && j1 < j2) {
+            for (int i = i1+1, j = j1+1, k = 0; k < totalRecorrer-1; i++, j++, k++) {
+                if (!tablero[i][j].contains(" ")) {
+                    atravesado = true;
+                    System.out.println("Hay alguien atravesado para poder llegar a la posición deseada (I = "+i+" J = "+j);
+                    break;
+                }
+            }
+        } else if (i1 < i2 && j1 > j2) {
+            for (int i = i1+1, j = j1-1, k = 0; k < totalRecorrer-1; i++, j--, k++) {
+                if (!tablero[i][j].contains(" ")) {
+                    atravesado = true;
+                    System.out.println("Hay alguien atravesado para poder llegar a la posición deseada (I = "+i+" J = "+j);
+                    break;
+                }
+            }
+        } else if (i1 > i2 && j1 < j2) {
+            for (int i = i1-1, j = j1+1, k = 0; k < totalRecorrer-1; i--, j++, k++) {
+                if (!tablero[i][j].contains(" ")) {
+                    atravesado = true;
+                    System.out.println("Hay alguien atravesado para poder llegar a la posición deseada (I = "+i+" J = "+j);
+                    break;
+                }
+            }
+        } else {
+            for (int i = i1-1, j = j1-1, k = 0; k < totalRecorrer-1; i--, j--, k++) {
+                if (!tablero[i][j].contains(" ")) {
+                    atravesado = true;
+                    System.out.println("Hay alguien atravesado para poder llegar a la posición deseada (I = "+i+" J = "+j);
+                    break;
+                }
+            }
+        }
+        if (atravesado == false) {
+            return true;
+        }
+        return false;
+    }
+
     private boolean puedeMoverAlfil(int i1, int j1, int i2, int j2, String tipo, String enemigo) {
         if (tablero[i2][j2].contains(tipo)) {
             if (i1 < i2 && j1 < j2) {
@@ -131,7 +214,7 @@ public class Juego {
         }
         return false;
     }
-    
+
     private boolean moverCaballo(int i1, int j1, int i2, int j2, String tipo) {
         if (i1 + 2 == i2 || i1 - 2 == i2) { //arriba-abajo
             if (j1 + 1 == j2 || j1 - 1 == j2) {
@@ -139,12 +222,12 @@ public class Juego {
                     tablero[i2][j2] = tablero[i1][j1];
                     tablero[i1][j1] = " ";
                     return true;
-                }else if (tipo.equals("B") && (tablero[i2][j2].contains("N") || tablero[i2][j2].contains(" "))) {
+                } else if (tipo.equals("B") && (tablero[i2][j2].contains("N") || tablero[i2][j2].contains(" "))) {
                     tablero[i2][j2] = tablero[i1][j1];
                     tablero[i1][j1] = " ";
                     return true;
                 }
-                
+
             }
         } else if (j1 + 2 == j2 || j1 - 2 == j2) { //derecha-izquierda
             if (i1 + 1 == i2 || i1 - 1 == i2) {
@@ -152,7 +235,7 @@ public class Juego {
                     tablero[i2][j2] = tablero[i1][j1];
                     tablero[i1][j1] = " ";
                     return true;
-                }else if (tipo.equals("B") && (tablero[i2][j2].contains("N") || tablero[i2][j2].contains(" "))) {
+                } else if (tipo.equals("B") && (tablero[i2][j2].contains("N") || tablero[i2][j2].contains(" "))) {
                     tablero[i2][j2] = tablero[i1][j1];
                     tablero[i1][j1] = " ";
                     return true;
@@ -160,9 +243,9 @@ public class Juego {
             }
         }
         return false;
-        
+
     }
-    
+
     private boolean moverPeonNegro(int i1, int j1, int i2, int j2) { //se suma la i
         int movimientoI = i1 + 1;
         if (tablero[i2][j2].equals(" ") && movimientoI == i2) {
@@ -176,29 +259,29 @@ public class Juego {
         }
         return false;
     }
-    
+
     private boolean moverPeonBlanco(int i1, int j1, int i2, int j2) { // se resta la I
         int movimientoI = i1 - 1;
         if (tablero[i2][j2].equals(" ") && movimientoI == i2) {
             tablero[i2][j2] = tablero[i1][j1];
             tablero[i1][j1] = " ";
             return true;
-        } else if (tablero[i2][j2].contains("B") && ((j1 + 1 == j2 || j1 - 1 == j2) && i1 - 1 == i2)) {
+        } else if (tablero[i2][j2].contains("N") && ((j1 + 1 == j2 || j1 - 1 == j2) && i1 - 1 == i2)) {
             tablero[i2][j2] = tablero[i1][j1];
             tablero[i1][j1] = " ";
             return true;
         }
         return false;
     }
-    
-    private boolean juegoTerminado(){
+
+    public boolean juegoTerminado() {
         int contadorBlancas = 0;
         int contadorNegras = 0;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (tablero[i][j].contains("B")) {
                     contadorBlancas++;
-                }else if (tablero[i][j].contains("N")) {
+                } else if (tablero[i][j].contains("N")) {
                     contadorNegras++;
                 }
             }
@@ -206,13 +289,13 @@ public class Juego {
         if (contadorBlancas == 0) {
             System.out.println("Ha ganado el Negro");
             return true;
-        }else if(contadorNegras == 0){
+        } else if (contadorNegras == 0) {
             System.out.println("Ha ganado el Blanco");
             return true;
         }
         return false;
     }
-    
+
     @Override
     public String toString() {
         String mensaje = "";
@@ -228,5 +311,5 @@ public class Juego {
         }
         return mensaje;
     }
-    
+
 }
--- Ajedrez/src/Main.java
+++ Ajedrez/src/Main.java
@@ -1,21 +1,52 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author valeria
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Juego jn = new Juego();
-        System.out.println(jn.toString());
+    private final Juego jn;
+    private final Scanner sc;
+
+    public Main() {
+        jn = new Juego();
+        sc = new Scanner(System.in);
     }
-    
+
+    public static void main(String[] args) {
+        Main juego = new Main();
+        juego.iniciar();
+    }
+
+    private void iniciar() {
+        System.out.println("Bienvenido al ajedrez.");
+        while (!jn.juegoTerminado()) {
+            while (jn.getTurno() % 2 == 0) { // turno de blanco
+                System.out.println(jn.toString());
+                do {
+                    System.out.println("Es el turno del blanco(B).\nPara mover una ficha digite su posición actual y la posición a usar. Ejemplo: 6 0 5 0");
+                    jn.moverFicha(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt()); //aca se mira si esta vacia o no es de tu tipo de ficha
+                    
+                } while (!jn.isMovimiento());
+
+            }
+            while (jn.getTurno() % 2 != 0) { //turno de negro
+                System.out.println(jn.toString());
+                do {
+                    System.out.println("Es el turno del negro(N).\nPara mover una ficha digite su posición actual y la posición a usar. Ejemplo: 6 0 5 0");
+                    jn.moverFicha(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt()); //aca se mira si esta vacia o no es de tu tipo de ficha
+                    
+                } while (!jn.isMovimiento());
+
+            }
+        }
+    }
+
 }
